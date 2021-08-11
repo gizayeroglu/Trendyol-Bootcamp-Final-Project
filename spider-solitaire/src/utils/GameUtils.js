a0f651185ccef519a9f6c02913707bfd2996cfff
Gizay Eroğlu <gizayeroglu1@hotmail.com>
2021-08-11 20:10:41 +0300

    get card holder with cards function added to get first 54 cards and ten more

--- spider-solitaire/src/utils/GameUtils.js
+++ spider-solitaire/src/utils/GameUtils.js
@@ -1,38 +1,42 @@
-export const getDeckData = () => {
-  const cardRank = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-  ];
+const cardRank = [
+  {rank:'A', count:8, value:1},
+  {rank:'2', count:8, value:2},
+  {rank:'3', count:8, value:3},
+  {rank:'4', count:8, value:4},
+  {rank:'5', count:8, value:5},
+  {rank:'6', count:8, value:6},
+  {rank:'7', count:8, value:7},
+  {rank:'8', count:8, value:8},
+  {rank:'9', count:8, value:9},
+  {rank:'10', count:8, value:10},
+  {rank:'J', count:8, value:11},
+  {rank:'Q', count:8, value:12},
+  {rank:'K', count:8, value:13}
+];
 
+export const getCardHoldersWithCards = (cardCount) => {
+  let cnt = 0;
   const data = [];
-  let count = 0;
 
-  while (count < 54) {
+  while(cnt < cardCount) {
     const symbolIndex = ~~(Math.random() * 13);
-    const cardHolderIndex = count % 10;
+    if (cardRank[symbolIndex].count === 0) continue;
+
+    const cardHolderIndex = cnt % 10;
 
     if (!data[cardHolderIndex]) {
       const cardHolder = { name: `cardholder${cardHolderIndex}`, cards: [] };
       data[cardHolderIndex] = cardHolder;
     }
-
+    
     data[cardHolderIndex].cards.push({
-      symbol: cardRank[symbolIndex],
-      isOpen: false,
-      value: symbolIndex + 1,
+      symbol: cardRank[symbolIndex].rank,
+      isOpen:false,
+      value: cardRank[symbolIndex].value
     });
-    count++;
+
+    cardRank[symbolIndex].count--; 
+    cnt++;
   }
 
   //Open last cards for every holder
@@ -42,26 +46,3 @@ export const getDeckData = () => {
 
   return data;
 };
-
-// /**Timer */
-// let totalSeconds = 0;
-// export const countTimer = () => {
-//   ++totalSeconds;
-//   let hour = ~~(totalSeconds / 3600);
-//   let minute = ~~((totalSeconds - hour*3600)/60);
-//   let seconds = totalSeconds - (hour*3600 + minute*60);
-//   if(hour < 10) {
-//     hour = '0' + hour;
-//   }
-//   if(minute < 10) {
-//     minute = '0' + minute;
-//   }
-//   if(seconds < 10) {
-//     seconds = '0' + seconds;
-//   }
-
-//   const time = `${hour} : ${minute} : ${seconds}`;
-//   return time;
-// }
-// let timer = setInterval(countTimer, 1000);
-// /**Timer */
