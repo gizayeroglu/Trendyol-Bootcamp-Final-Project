ff5db12e9941d1d83b0aa91b66c723e490f23863
Gizay Eroğlu <gizayeroglu1@hotmail.com>
2021-08-11 20:09:12 +0300

    home page header title changed

--- spider-solitaire/src/components/Header/Header.js
+++ spider-solitaire/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "../Header/Header.css";
 import React from "react";
 
 function Header() {
-  return <h1 className='header'>Spider Solitaire</h1>;
+  return <h1 className='header'>Reversed Spider Solitaire</h1>;
 }
 
 export default Header;
