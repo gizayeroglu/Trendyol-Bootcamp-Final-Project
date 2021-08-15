e7014107c1d2ce0cd99880980d8bcdad05852d63
Gizay Eroğlu <gizayeroglu1@hotmail.com>
2021-08-15 17:50:06 +0300

    card distributor component tests completed

--- spider-solitaire/src/__test__/components/CardDistributor.test.js
+++ spider-solitaire/src/__test__/components/CardDistributor.test.js
@@ -1,19 +1,28 @@
-import React from "react";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import { configure, shallow, mount } from "enzyme";
+import React from 'react';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { configure, shallow } from 'enzyme';
 
-import CardDistributor from "../../components/CardDistributor/CardDistributor";
+import CardDistributor from '../../components/CardDistributor/CardDistributor';
 
 configure({ adapter: new Adapter() });
 
-describe("Card Distrubitor component tests", () => {
+describe('Card Distrubitor component tests', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(<CardDistributor  />);
   });
 
-  it("should render the card holder component", () => {
+  it('should render the card holder component', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should have clickable cards to deal with new 10 cards', () => {
+    const getNewCards = jest.fn();
+    const wrapper = shallow(<CardDistributor getNewCards={getNewCards}/>)
+    expect(getNewCards).toHaveBeenCalledTimes(0);
+    wrapper.find('.card-distrubitor-container').children().at(1).simulate('click');
+    expect(getNewCards).toHaveBeenCalledTimes(1);
+  });
+
 });
