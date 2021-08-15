a88f5e61c8d6bb0bcd5b274e0368ae82dfea0b6a
Gizay Eroğlu <gizayeroglu1@hotmail.com>
2021-08-15 15:53:00 +0300

    Ten cypress tests completed

--- spider-solitaire/cypress/integration/sample_spec.js
+++ spider-solitaire/cypress/integration/sample_spec.js
@@ -8,7 +8,6 @@ describe('Home page tests', () => {
     })
 
     it('should redirect to game page when the play game card div clicked', () => {
-
         cy.get('.play-game-card').click()
         cy.location('pathname').should('equal', '/play');
 
@@ -18,13 +17,52 @@ describe('Home page tests', () => {
 describe('Game page tests', () => {
 
     it('should have score and time text on the top of the page', () => {
-
         cy.contains('Score');
         cy.contains('Time');
     })
 
-    it('should redirect to home page when the home button clicked', () => {
+    it('should have visible game area container which display style is grid', () => {
+       cy.get('.game-area-container')
+       .should('be.visible')
+       .should('have.css', 'display')
+       .and('match', /grid/); 
+    });
 
+    it('should have visible card holder which display style is flex', () => {
+       cy.get('.game-area-container').children().first()
+       .should('be.visible')
+       .should('have.css', 'display')
+       .and('match', /flex/);
+    });
+
+    it('should have visible last card of the holder and the last card have background color because it should be a card-front', () => {
+       cy.get('.game-area-container').children().first().children().last()
+       .should('be.visible')
+       .should('have.css', 'background-color')
+    });
+
+    it('should have visible first card of the holder and first card have background URL because it should be a card-back', () => {
+       cy.get('.game-area-container').children().first().children().first()
+       .should('be.visible')
+       .should('have.css', 'background')
+       .and('match', /url()/);
+    });
+
+    it('should have game area top containers at the top which display style is flex', () => {
+       cy.get('.game-area-top-containers')
+       .should('be.visible')
+       .should('have.css', 'display')
+       .and('match', /flex/);
+    });
+
+    it('should have card distributor as the last child of the game-area-top-containers and display style should be grid', () => {
+       cy.get('.game-area-top-containers').children().last()
+       .should('be.visible')
+       .should('have.css', 'display')
+       .and('match', /grid/);
+    });
+
+    it('should redirect to home page when the home button clicked', () => {
         cy.get('.go-back-button').click({ force: true })
         cy.location('pathname').should('equal', '/');
     })
