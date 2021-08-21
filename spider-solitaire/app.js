c20fb492cd013e2f3a068164e2b51685095f331c
Gizay Eroğlu <gizayeroglu1@hotmail.com>
2021-08-21 04:26:49 +0300

    play page

--- spider-solitaire/app.js
+++ spider-solitaire/app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/', express.static(path.join(__dirname, 'build')))
+app.use('/play', express.static(path.join(__dirname, 'build')))
 
 app.listen(port, (err) => {
   if(err) return console.log(err);
